f1cfb8ede6aa0f3795d85207867b4d5359d9a52c
amelia.brown <amelia.brown@MU-C1-09.mullen.pvt.k12.co.us>
2018-09-07 14:58:21 -0600

    created project

---
+++ DigitStripping/src/DigitStripping.java
@@ -0,0 +1,36 @@
+
+public class DigitStripping {
+
+	public static void main(String[] args) {
+	challengeOne();
+	challengeTwo();
+	challengeThree();
+	challengeFour();
+	challengeFive();
+	}
+
+	public static void challengeOne()
+	{
+		
+	}
+
+	public static void challengeTwo()
+	{
+		
+	}
+	
+	public static void challengeThree()
+	{
+		
+	}
+
+	public static void challengeFour()
+	{
+		
+	}
+
+	public static void challengeFive()
+	{
+		
+	}
+}
