358b16cd8b2c9b65fe1fbc8c14b23141b3a06132
amelia.brown <amelia.brown@MU-C1-09.mullen.pvt.k12.co.us>
2018-09-12 10:19:44 -0600

    finished challenge two

--- DigitStripping/src/DigitStripping.java
+++ DigitStripping/src/DigitStripping.java
@@ -1,36 +1,62 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthScrollBarUI;
 public class DigitStripping {
 
 	public static void main(String[] args) {
 	challengeOne();
-	challengeTwo();
-	challengeThree();
-	challengeFour();
-	challengeFive();
+	challengeTwoThroughFive();
 	}
 
 	public static void challengeOne()
 	{
+		int[] userNumbers = new int [6];
+		System.out.println("Please enter a six digit number.");
+		Scanner userInput = new Scanner(System.in);
 		
+		int userNumber = userInput.nextInt();
+		for (int i = 0; i < userNumbers.length; i++)
+		{
+			int digitStripping = userNumber%10;
+			userNumbers[i] = digitStripping;
+			userNumber = userNumber/10;
+			
+		}
+		System.out.println(Arrays.toString(userNumbers));
 	}
 
-	public static void challengeTwo()
-	{
-		
-	}
 	
-	public static void challengeThree()
+	public static void challengeTwoThroughFive()
 	{
+		Scanner secondUserInput = new Scanner (System.in);
+		System.out.println("Please enter a number.");
+		int inputLength = secondUserInput.nextInt();
 		
+		int counter = 0;
+		while (inputLength > 0)
+		{
+			inputLength = inputLength/10;
+			counter++;	
+		}
+		System.out.println("There are " + counter + " digits in your number.");
+
+
+		
+	
+		int[] userNumbers2 = new int [6];
+		
+		int userNumber2 = inputLength.nextInt();
+		for (int i = 0; i < userNumbers2.length; i++)
+		{
+			int digitStripping = userNumber2%10;
+			userNumbers2[i] = digitStripping;
+			userNumber2 = userNumber2/10;
+			
+		}
+		System.out.println(Arrays.toString(userNumbers2));
 	}
 
-	public static void challengeFour()
-	{
-		
+	
 	}
 
-	public static void challengeFive()
-	{
-		
-	}
-}
